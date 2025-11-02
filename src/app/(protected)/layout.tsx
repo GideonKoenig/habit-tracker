ff56d579433de48126eaf1924fca573401319914
Gideon Koenig <gideon.koenig@online.de>
2025-11-02 22:24:48 +0100

    feat: add scrollarea wrapper

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { getUser } from "@/server/auth";
 import AppHeader from "@/components/app-header";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default async function ProtectedLayout(
     props: Readonly<{ children: React.ReactNode }>,
@@ -9,11 +10,13 @@ export default async function ProtectedLayout(
     if (!user) redirect("/login");
 
     return (
-        <div className="mx-auto h-full w-full max-w-5xl">
-            <AppHeader />
-            <main className="flex flex-col gap-10 px-8 py-12">
-                {props.children}
-            </main>
-        </div>
+        <ScrollArea className="h-full w-full">
+            <div className="mx-auto h-dvh w-full max-w-5xl">
+                <AppHeader />
+                <main className="flex flex-col gap-10 px-8 py-12">
+                    {props.children}
+                </main>
+            </div>
+        </ScrollArea>
     );
 }
